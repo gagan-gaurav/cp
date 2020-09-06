d41aaef908834af59310061c918ff649475cd1aa
gagan-gaurav <iamgagangaurav@gmail.com>
2020-09-06 14:31:47 +0530

    using stack

--- algorithms/amortized_analysis/nearest_smaller_elememnt.cpp
+++ algorithms/amortized_analysis/nearest_smaller_elememnt.cpp
@@ -10,18 +10,19 @@ void print( T(&theArray)[N]  ) {
 }
 
 int main(){
-    int a[13] = {2, 3, 50, 4, 5, 20, 6, 61, 23, 45, 34, 22, 12};
+    int a[13] = {2, 3, 50, 4, 5, 20, 6, 61, 23, 45, 34, 22, 1};
     int b[13] = {0};
     stack<int> s;
     s.push(-1);
-    // for(int i = 0; i < 13; i++){
-    //     if(!s.empty()){
-    //         while(a[i] < s.top()){
-    //             s.pop();
-    //         }
-    //         else{
-    //     }
-    // }
+    for(int i = 0; i < 13; i++){
+        if(!s.empty()){
+            while(a[i] < s.top()){
+                s.pop();
+            }
+        }
+        b[i] = s.top();
+        s.push(a[i]);
+    }
     print(b);
     return 0;  
 }

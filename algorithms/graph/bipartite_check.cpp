57555b56a07395c4e40fa9f67e60f3267f9b5f80
gagan-gaurav <iamgagangaurav@gmail.com>
2021-01-17 12:43:00 +0530

    bipartite check

---
+++ algorithms/graph/bipartite_check.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> adj;
+
+void bipartite_check(int n, int s) {
+    bool is_bipartite = true;
+    queue<int> q;
+    vector<int> color(n + 1, -1);
+    q.push(s);
+    color[s] = 0;
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+        for (auto &i : adj[v]) {
+            if (color[i] == -1) {
+                color[i] = color[v] ^ 1;
+                q.push(i);
+            } else {
+                is_bipartite &= color[i] != color[v];
+            }
+        }
+    }
+    is_bipartite ? cout << "YES" << '\n' : cout << "NO" << '\n';
+    for (auto &i : color) cout << i << " ";
+}
+
+int main() {
+    ios_base ::sync_with_stdio(false);
+    cin.tie(0);
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i <= n; i++) adj.push_back(vector<int>());
+    for (int i = 0; i < m; i++) {
+        int x, y;
+        cin >> x >> y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    bipartite_check(n, 1);
+    return 0;
+}

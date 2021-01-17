bb8810a367898f528c5fa5f85d515d7206f81968
gagan-gaurav <iamgagangaurav@gmail.com>
2021-01-17 11:39:06 +0530

    ..

--- algorithms/graph/bfs.cpp
+++ algorithms/graph/bfs.cpp
@@ -1,21 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
- // bfs algorithm //
+// bfs algorithm //
 
+vector<vector<int>> adj;
 queue<int> q;
-bool visited[N];
-int distance[N];
 
-visited[x] = true;
-distance[x] = 0;
-q.push(x);
-while(!q.empty()){
-  int s = q.front(); q.pop();
-  for(auto u : adj[s]){
-    if(visited[u]) continue;
-    visited[u] = true;
-    distance[u] = distance[s] + 1;
-    q.push(u);
-  }
+void bfs(int n, int s, vector<int> &d, vector<int> &p) {
+    vector<bool> visited(n + 1, false);
+    d.assign(n + 1, 0);
+    p.assign(n + 1, -1);
+    visited[s] = true;
+    q.push(s);
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+        for (auto &i : adj[v]) {
+            if (!visited[i]) {
+                visited[i] = true;
+                q.push(i);
+                d[i] = d[v] + 1;
+                p[i] = v;
+            }
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i <= n; i++) adj.push_back(vector<int>());
+    for (int i = 0; i < m; i++) {
+        int x, y;
+        cin >> x >> y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    vector<int> d, p;
+    bfs(n, 1, d, p);
+    for (auto &i : d) cout << i << ' ';
+    return 0;
 }

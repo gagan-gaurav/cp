18af7a85375151ec783eae30da0c1df88dbd0112
gagan-gaurav <iamgagangaurav@gmail.com>
2021-08-14 05:04:50 +0530

    //

--- algorithms/range_queries/sparse_table.cpp
+++ algorithms/range_queries/sparse_table.cpp
@@ -14,7 +14,7 @@ void rsq(){                    // O(N*logN)
 	for(int i = 0; i < a.size(); i++) st[i][0] = a[i];
 	for(int j = 1; j <= K; j++){
 		for(int i = 0; i + (1 << j) <= a.size(); i++)
-			st[i][j] = st[i][j - 1] + st[i + (1 << (j - 1))][j - 1]; //  st[i][j] = f(st[i][j - 1], st[i + (1 << (j - 1))][j - 2])
+			st[i][j] = st[i][j - 1] + st[i + (1 << (j - 1))][j - 1]; //  st[i][j] = f(st[i][j - 1], st[i + (1 << (j - 1))][j - 1])
 	}
 }
 
@@ -30,9 +30,11 @@ ll sum_query(int L, int R){          // O(logN)
 }
 
 
-/// rang minimum query implementation
 
 vector<int> lg(MAXN + 1);  //log value preprocessing
+
+
+/// range minimum query implementation
 
 void rmq(){                       // O(N*logN)
 	lg[1] = 0;
@@ -41,7 +43,7 @@ void rmq(){                       // O(N*logN)
 	for(int i = 0; i < a.size(); i++) st[i][0] = a[i];
 	for(int j = 1; j <= K; j++){
 		for(int i = 0; i + (1 << j) <= a.size(); i++)
-			st[i][j] = min(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);  //  st[i][j] = f(st[i][j - 1], st[i + (1 << (j - 1))][j - 2])
+			st[i][j] = min(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);  //  st[i][j] = f(st[i][j - 1], st[i + (1 << (j - 1))][j - 1])
 	}
 }
 
@@ -49,6 +51,26 @@ int min_query(int L, int R){       // O(1)
 	int j = lg[R - L + 1];
 	int minimum = min(st[L][j], st[R - (1 << j) + 1][j]);
 	return minimum;
+}
+
+
+/// range gcd query
+
+void rgq(){							// O(N*logN)
+	lg[1] = 0;
+	for(int i = 2; i <= MAXN; i++) lg[i] = lg[i / 2] + 1;
+
+	for(int i = 0; i < (int)a.size(); i++) st[i][0] = a[i];
+	for(int j = 1; j <= K; j++){
+		for(int i = 0; i + (1 << j) <= a.size(); i++){
+			st[i][j] = __gcd(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
+		}
+	}
+}
+
+int gcd_query(int L, int R){
+	int j = lg[R - L + 1];
+	return __gcd(st[L][j], st[R - (1 << j) + 1][j]);
 }
 
 
